48585ad7b48985964d8e8421aac0c3d6dde97ae5
Ray <82468210+protogenraymond@users.noreply.github.com>
2021-11-06 16:36:57 -0400

    fixed the messy spaghetti code

--- ProductKeyGenerator.js
+++ ProductKeyGenerator.js
@@ -1,28 +1,14 @@
-(async function exampleModule() {
-var search = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);  
-var search1 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search2 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search3 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search4 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search5 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);  
-var search6 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search7 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search8 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search9 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search10 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search11 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);  
-var search12 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search13 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search14 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search15 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search16 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);  
-var search17 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search18 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search19 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search20 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search21 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);  
-var search22 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search23 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-var search24 = $io.arr.random(['A','B','C','D','E','F','G','H','I J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9']);
-$alert({msg:" " + search.replace() + search1.replace() + search2.replace() + search3.replace() + search4.replace() + "-" + search5.replace() + search6.replace() + search7.replace() + search8.replace() + search9.replace() + "-" + search10.replace() + search11.replace() + search12.replace() + search13.replace() + search14.replace() + "-" + search15.replace() + search16.replace() + search17.replace() + search18.replace() + search19.replace() + "-" + search20.replace() + search21.replace() + search22.replace() + search23.replace() + search24.replace(),center:!1,btnOk:"OK",title:"Your Product Key Is...",animationOut:"none",icon:"https://i.imgur.com/zzz1KA1.png"})  
+(async function productKey() {
+  function MakeId(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+var product = MakeId(5) + '-' + MakeId(5) + '-' + MakeId(5) + '-' + MakeId(5) + '-' + MakeId(5)
+$alert(`Your Windows93 product key is ${product}`);
 })
